b93133f7358adc0366f8e35909cb90b29dbdf67a
Baptiste Béghin <baptistebeghin@hotmail.fr>
2017-06-20 15:38:23 +0200

    Etape 6 : ajout du Cache

--- src/java/fr/paris/lutece/plugins/example/service/ProjectCacheService.java
+++ src/java/fr/paris/lutece/plugins/example/service/ProjectCacheService.java
@@ -1,12 +1,15 @@
 package fr.paris.lutece.plugins.example.service;
 
 import fr.paris.lutece.plugins.example.business.Project;
+import fr.paris.lutece.plugins.example.business.ProjectHome;
 import fr.paris.lutece.portal.service.cache.AbstractCacheableService;
+import fr.paris.lutece.portal.service.security.LuteceUser;
 
 public class ProjectCacheService extends AbstractCacheableService {
 
 	
 	public static final String SERVICE_NAME = "Project Service";
+	private static ProjectCacheService _instance= new ProjectCacheService();
 	
 	public ProjectCacheService(){
 		initCache();
@@ -16,24 +19,39 @@ public class ProjectCacheService extends AbstractCacheableService {
 	public String getName() {
 		return SERVICE_NAME;
 	}
+	
+	public static ProjectCacheService getInstance(){
+		return _instance;
+	}
 
-	public Project getResource(String strId){
+	public Project getResource(String strId, LuteceUser user){
 		
-		Project r = (Project) getFromCache(strId);
+		String cacheKey = getCacheKey(strId, user);
+		
+		Project r = (Project) getFromCache(cacheKey);
 		
 		if(r==null){
-			r=getResourceFromSource(strId);
-			putInCache(strId, r);
+			r = ProjectHome.findByPrimaryKey(Integer.parseInt(strId));
+			
+			cacheKey = getCacheKey(String.valueOf(r.getId()),user);
+			putInCache(cacheKey, r);
 		}
 		
 		return r;
 	}
 	
-	private static String getCacheKey(String strId, LuteceUser user){
-		
-		StringBuilder sbKey = new StringBuilder();
-		sbKey.append( "[res:" ).append( strId ).append( "][user:").append( user.getName() ).append("]");
-	     return sbKey.toString();
-	}
+	private static String getCacheKey(String strId, LuteceUser user) {
+        StringBuilder sbKey = new StringBuilder();
+        sbKey.append("[project:").append(strId).append("][user:");
+
+        if (user != null) {
+            sbKey.append(user.getName());
+        } else {
+            sbKey.append("public");
+        }
+        sbKey.append("]");
+
+        return sbKey.toString();
+    }
 	
 }
